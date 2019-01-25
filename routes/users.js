2e06e71976ef34de460b66d125bce0247f96dec7
翟林 <18731810122@163.com>
2019-01-25 13:17:10 +0800

    .

--- routes/users.js
+++ routes/users.js
@@ -112,7 +112,6 @@ router.post('/update', (req, res) => {
 //查询聊天信息
 router.get('/getmsglist', (req, res) => {
   const userid = req.cookies.userid;
-  // console.log(userid)
   User.find({}, (err, userdoc) => {
     let users = {};
     userdoc.forEach( v => {
@@ -123,7 +122,6 @@ router.get('/getmsglist', (req, res) => {
     })
     Chat.find({"$or": [{from: userid}, {to: userid}]}, (err, doc) => {
       if (!err) {
-        // console.log(doc)
         return res.json({
           code: 0,
           data: doc,
@@ -144,7 +142,6 @@ router.post('/readmsg', (req, res) => {
     {'multi':true},
     (err, doc) => {
       if (!err) {
-        // console.log(doc)
         return res.json({
           code: 0,
           num: doc.nModified
